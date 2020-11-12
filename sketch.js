e6a7e61ce7378a04af5fd077a73de068888318e3
peter <pjbaldes@vcu.edu>
2020-11-12 16:30:48 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var stem;
 var flowerc; 
 let i;
 let s;
+var petalCount;
 
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight);
@@ -41,6 +42,9 @@ function grow() {
     x=x+random(-.5, .5);
     y=y+random(-.5, .5);
   } else if (r>.998) {
+     jitterr=random(100, 235);
+     jitterg=random(100, 235);
+     jitterb=random(100, 235);
     startFlower=true;
   }
 }
@@ -55,22 +59,43 @@ function locate() {
 
   startFlower=false;
   flowerCount=0;
+  petalCount=0;
 }
 
 function flower() {
+
+  flowerc = color(jitterr+random(-30, 20), jitterg+random(-30, 20), jitterb+random(-30, 20));
+  s=random(flowerCount/20); 
+  if (petalCount<flowerCount/20) {
+    stroke(flowerc);
+    fill(flowerc);
+    ellipse(x+random(-s/2, s/2), y+random(-s/2, s/2), random(s/2), random(s/2));
+    petalCount++;
+    
+  } else {
+    locate();
+  }
+}
+function flower2() {
   var jitterr=random(100, 235);
   var jitterg=random(100, 235);
   var jitterb=random(100, 235);
   flowerc = color(jitterr+random(-20, 20), jitterg+random(-20, 20), jitterb+random(-20, 20));
   s=random(flowerCount/20); 
-  for (i=0; i<50; i++) {
-    stroke(flowerc);
-    fill(flowerc);
-    ellipse(x+random(-s, s), y+random(-s, s), random(s/2), random(s/2));
-    flowerc = color(jitterr+random(-30, 20), jitterg+random(-30, 20), jitterb+random(-30, 20));
+  for (i=0; i<flowerCount/10; i++) {
+    var xf=random(x+random(-s, s));
+    var yf=random(y+random(-s, s));
+    var d=dist(x, y, xf, yf);
+    if (d>flowerCount/20) {
+      xf=random(x+random(-s, s));
+      yf=random(y+random(-s, s));
+    } else {
+      stroke(flowerc);
+      fill(flowerc);
+      ellipse(x+random(-s, s), y+random(-s, s), random(s/2), random(s/2));
+      flowerc = color(jitterr+random(-30, 20), jitterg+random(-30, 20), jitterb+random(-30, 20));
+    }
   }
-
-
 
   locate();
 }
